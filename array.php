2c5a1e8bd0575c1b92e79a1089ded5428f88d00e
Eza51 <nowshinnjerineza51@gmail.com>
2025-05-18 22:20:28 +0600

    Add/update specific file

---
+++ array.php
@@ -0,0 +1,26 @@
+<?php
+// Mastering Laravel with Eza
+
+//Array
+$num=20;
+$name=array();
+//echo 'empty array'. '<br>';
+//print_r($name);
+//arraylast elemt =  3-1;
+$names=['cat','dog' ,'birds','butterfly'];
+//print_r($names);
+// echo $names [3];
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+?>
