3d693b5884030947086f11b924ede974005cb654
Eza51 <nowshinnjerineza51@gmail.com>
2025-07-04 23:28:55 +0600

    Add oop_class5.php

---
+++ oop_class5.php
@@ -0,0 +1,14 @@
+<?php
+//Mastering laravel With Eza
+class u{
+    public $color;
+    public $consistancy;
+    public function __construct($color,$consistancy){
+    echo "u is" . $color . $consistancy;
+    }
+}
+
+$newgu = new u("yellor","pok");
+
+
+?>
